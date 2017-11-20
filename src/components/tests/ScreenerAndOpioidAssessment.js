9139611af0c8f2d44366325cf735e9ddb849464a
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-11-20 14:29:09 -0800

    PHQ-9 assessment rendering all question types(styling)

--- src/components/tests/ScreenerAndOpioidAssessment.js
+++ src/components/tests/ScreenerAndOpioidAssessment.js
@@ -72,7 +72,8 @@ class ScreenerAndOpioidAssessment extends Component {
 		const packageJSON = [];
 		return (
 			<ScrollView>
-				<Text style={{ margin: 5, fontSize: 20 }}>Quality of Life Questionnaire</Text>
+				<Text style={{ margin: 5, fontSize: 20 }}>Screener and Opioid Assessment for Patients with Pain-Revised</Text>
+				<Text style={{ margin: 5, fontSize: 14 }}>The following are some questions given to patients who are on or being considered for medication for their pain. Please answer each question as honestly as possible. There are no right or wrong answers.</Text>
 				<FlatList
 					data={newList.content}
 					keyExtractor={(x, i) => i}
@@ -1041,196 +1042,14 @@ class ScreenerAndOpioidAssessment extends Component {
 		const newList = {
 			content: list
 		};
+		console.log('newList', newList);
 		// console.log('new_list.content', newList.content);
 		const packageJSON = [];
 
 
 		return (
-			<View style={{ paddingBottom: 400, justifyContent: 'center' }}>
-				<ScrollView>
-					<Text style={{ margin: 5 }}>Screener and Opioid Assessment for Patients with Pain-Revised (SOAPP®-R)</Text>
-					<Text style={{ margin: 5 }}>
-						The following are some questions given to patients who are on or being considered for medication for their pain. Please answer each question as honestly as possible. There are no right or wrong answers.
-					</Text>
-					<View
-						style={{
-							flexDirection: 'column',
-							justifyContent: 'space-between',
-							paddingLeft: 4
-						}}
-					>
-						<CheckBox
-							label='0 Never'
-						/>
-						<CheckBox
-							label='1 Seldom'
-						/>
-						<CheckBox
-							label='2 Sometimes'
-						/>
-						<CheckBox
-							label='3 Often'
-						/>
-						<CheckBox
-							label='4 Very Often'
-						/>
-					</View>
-					<FlatList 
-						data={newList.content}
-						keyExtractor={(x, i) => i}
-						// renderItem={({ item, index }) => 
-							// console.log('index and item.question ', index, item.question)}
-						renderItem={({ item, index }) => 
-							<View style={{ padding: 10 }}>
-								<Text style={{ padding: 10, fontSize: 22 }}>
-									{index + 1}) {item.question}
-								</Text>
-								<View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-									<CheckBox
-										name={item.question}
-										label='0'
-										onChange={(checked) => {
-											console.log('I am checked yes', checked);
-											console.log('I am checked yes initial state name', this.state.name);
-											if (this.state.checkedYes) {
-												this.setState({
-													checkedYes: false,
-													checkedNo: false,
-													name: null
-												});
-											} else {
-												this.setState({ 
-													checkedYes: true,
-													checkedNo: false,
-													name: item.question
-												});	
-											}
-											console.log('I am checked yes @_@', checked);
-											console.log('I am checked yes initial state name @_@', this.state.name);
-										}}
-									/>
-									<CheckBox
-										name={item.question}
-										label='1'
-										onChange={(checked) => {
-											console.log('I am checked no', checked);
-											console.log('I am checked no initial state name', this.state.name);
-											if (this.state.checkedNo) {
-												this.setState({
-													checkedNo: false,
-													checkedYes: false,
-													name: null
-												});
-											} else {
-												this.setState({
-													checkedNo: true,
-													checkedYes: false,
-													name: item.question
-												});
-											}
-											console.log('I am checked no @_@', checked);
-											console.log('I am checked no initial state name @_@', this.state.name);
-										}}
-									/>
-									<CheckBox
-										name={item.question}
-										label='2'
-										onChange={(checked) => {
-											console.log('I am checked yes', checked);
-											console.log('I am checked yes initial state name', this.state.name);
-											if (this.state.checkedYes) {
-												this.setState({
-													checkedYes: false,
-													checkedNo: false,
-													name: null
-												});
-											} else {
-												this.setState({ 
-													checkedYes: true,
-													checkedNo: false,
-													name: item.question
-												});	
-											}
-											console.log('I am checked yes @_@', checked);
-											console.log('I am checked yes initial state name @_@', this.state.name);
-										}}
-									/>
-									<CheckBox
-										name={item.question}
-										label='3'
-										onChange={(checked) => {
-											console.log('I am checked yes', checked);
-											console.log('I am checked yes initial state name', this.state.name);
-											if (this.state.checkedYes) {
-												this.setState({
-													checkedYes: false,
-													checkedNo: false,
-													name: null
-												});
-											} else {
-												this.setState({ 
-													checkedYes: true,
-													checkedNo: false,
-													name: item.question
-												});	
-											}
-											console.log('I am checked yes @_@', checked);
-											console.log('I am checked yes initial state name @_@', this.state.name);
-										}}
-									/>
-									<CheckBox
-										name={item.question}
-										label='4'
-										onChange={(checked) => {
-											console.log('I am checked yes', checked);
-											console.log('I am checked yes initial state name', this.state.name);
-											if (this.state.checkedYes) {
-												this.setState({
-													checkedYes: false,
-													checkedNo: false,
-													name: null
-												});
-											} else {
-												this.setState({ 
-													checkedYes: true,
-													checkedNo: false,
-													name: item.question
-												});	
-											}
-											console.log('I am checked yes @_@', checked);
-											console.log('I am checked yes initial state name @_@', this.state.name);
-										}}
-									/>
-								</View>
-							</View>
-						}
-						// <Text>{item.question} id:{index}</Text>}
-							// console.log('flatlist item ', item.question);
-					/>
-					<Button
-						style={{ paddingTop: 20 }}
-						onPress={() => {
-							console.log('newText ', this.state.tempValue);
-							const pack = {
-								message: 'Finished',
-								assessment: this.props.assessment,
-								patient: this.props.patient,
-								question: this.state.tempValue
-							};
-							console.log('pack', pack);
-							axios.post('https:lags-assessments-mobileapp-api.herokuapp.com/api/v1/lagz_forms/assessments/answers', pack)
-								.then((response) => {
-									console.log('response!', response.data);
-									if (response.data.data === 'Finished') {
-										console.log('Finished');
-										this.props.setPage;
-									}
-								});
-						}}
-					>
-						Done
-					</Button>
-				</ScrollView>
+			<View style={{ paddingBottom: 400 }}>
+				{this.renderQuestionType(newList)}
 			</View>
 		);
 	}
